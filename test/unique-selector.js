d7b00a7b6cda7c31dd916e7191aed00aa8d41678
Eric Clemmons <eric@smarterspam.com>
2013-03-27 11:18:43 -0500

    Fix nth-child calculation

--- test/unique-selector.js
+++ test/unique-selector.js
@@ -24,14 +24,19 @@ describe('unique-selector', function() {
     });
 
     var selectors = {
+        // Selector: expected
         'HTML > BODY':
             'html > body',
         'HTML > BODY > DIV#fixture':
             '#fixture',
         'HTML > BODY > DIV#fixture > H3 > SMALL':
             '#fixture > h3 > small',
-        'HTML > BODY > DIV#fixture > UL#nav > LI.item:nth-child(2)':
-            '#nav > li.item:nth-child(2)',
+        'HTML > BODY > DIV#fixture > UL#nav > LI:nth-child(1)':
+            '#nav > li.first.item',
+        'HTML > BODY > DIV#fixture > UL#nav > LI:nth-child(2)':
+            '#nav > li.collapsed.item:nth-child(2)',
+        'HTML > BODY > DIV#fixture > UL#nav > LI:nth-child(3)':
+            '#nav > li.last.collapsed.item',
         'HTML > BODY > DIV#fixture > UL#nav > LI.item > UL#nested > LI.child > IMG':
             '#nested > li.child > img[alt="Some Title"]'
     };
