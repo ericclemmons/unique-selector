f048b3b19f818ae8b582fda0eda97cfcdff14a22
nkashyap <nisheeth.k.kashyap@gmail.com>
2016-11-11 06:24:46 +0000

    exclude HTML element for css selector

--- src/isElement.js
+++ src/isElement.js
@@ -5,13 +5,17 @@ export function isElement( el )
 {
   let isElem;
 
-  if ( typeof HTMLElement === 'object' )
+  if ( el && el.tagName !== 'HTML' )
   {
-    isElem = el instanceof HTMLElement;
+    if ( typeof HTMLElement === 'object' )
+    {
+      isElem = el instanceof HTMLElement;
+    }
+    else
+    {
+      isElem = !!el && ( typeof el === 'object' ) && el.nodeType === 1 && typeof el.nodeName === 'string';
+    }
   }
-  else
-  {
-    isElem = !!el && ( typeof el === 'object' ) && el.nodeType === 1 && typeof el.nodeName === 'string';
-  }
+
   return isElem;
 }
